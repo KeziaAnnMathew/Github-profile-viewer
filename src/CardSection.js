713b7fcaa082e90a54530d5b5d0c8eaaa9e78748
KeziaAnnMathew <keziaann26@gmail.com>
2021-05-26 01:57:42 +0530

    SearchPage without lazy loading

--- src/CardSection.js
+++ src/CardSection.js
@@ -1,111 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 import {Card} from 'react-bootstrap';
 import {Person} from '@material-ui/icons';
+import Users from './Users.json';
 import './CardSection.css';
 
-class CardSection extends Component {
-    render(){
+// class CardSection extends Component {
+    const CardSection= (props) => {
         return(
             <React.Fragment>
                 <div className="card-container">
-                <Card className="card" style={{ width: '19rem', height:'9rem' }}>
-                    <Card.Body className="card-body">
-                        <div className="user-img">
-                            <p className="person-icon"><Person /></p>
-                        </div>
-                        <div className="user-details">
-                        <Card.Title>User name</Card.Title>
-                        <Card.Text className="text">
-                            @handle, work at description<br/>
-                            bio to be wrap and not than <br/>
-                            3 lines
-                        </Card.Text>
-                        </div>
-                    </Card.Body>
-                </Card>
-                <Card className="card" style={{ width: '19rem', height:'9rem' }}>
-                    <Card.Body className="card-body">
-                        <div className="user-img">
-                            <p className="person-icon"><Person /></p>
-                        </div>
-                        <div className="user-details">
-                        <Card.Title>User name</Card.Title>
-                        <Card.Text className="text">
-                            @handle, work at description<br/>
-                            bio to be wrap and not than <br/>
-                            3 lines
-                        </Card.Text>
-                        </div>
-                    </Card.Body>
-                </Card>
-                <Card className="card" style={{ width: '19rem', height:'9rem' }}>
-                    <Card.Body className="card-body">
-                        <div className="user-img">
-                            <p className="person-icon"><Person /></p>
-                        </div>
-                        <div className="user-details">
-                        <Card.Title>User name</Card.Title>
-                        <Card.Text className="text">
-                            @handle, work at description<br/>
-                            bio to be wrap and not than <br/>
-                            3 lines
-                        </Card.Text>
-                        </div>
-                    </Card.Body>
-                </Card>
-                <Card className="card" style={{ width: '19rem', height:'9rem' }}>
-                    <Card.Body className="card-body">
-                        <div className="user-img">
-                            <p className="person-icon"><Person /></p>
-                        </div>
-                        <div className="user-details">
-                        <Card.Title>User name</Card.Title>
-                        <Card.Text className="text">
-                            @handle, work at description<br/>
-                            bio to be wrap and not than <br/>
-                            3 lines
-                        </Card.Text>
-                        </div>
-                    </Card.Body>
-                </Card>
-                <Card className="card" style={{ width: '19rem', height:'9rem' }}>
-                    <Card.Body className="card-body">
-                        <div className="user-img">
-                            <p className="person-icon"><Person /></p>
-                        </div>
-                        <div className="user-details">
-                        <Card.Title>User name</Card.Title>
-                        <Card.Text className="text">
-                            @handle, work at description<br/>
-                            bio to be wrap and not than <br/>
-                            3 lines
-                        </Card.Text>
-                        </div>
-                    </Card.Body>
-                </Card>
-                <Card className="card" style={{ width: '19rem', height:'9rem' }}>
-                    <Card.Body className="card-body">
-                        <div className="user-img">
-                            <p className="person-icon"><Person /></p>
-                        </div>
-                        <div className="user-details">
-                        <Card.Title>User name</Card.Title>
-                        <Card.Text className="text">
-                            @handle, work at description<br/>
-                            bio to be wrap and not than <br/>
-                            3 lines
-                        </Card.Text>
-                        </div>
-                    </Card.Body>
-                </Card>
-                
-
-                
+                    {Users.users.filter((val)=>{
+                        if(props.term === ""){
+                            return val;
+                        }
+                        else if(val.userName.toLowerCase().includes(props.term.toLowerCase())){
+                            return val;
+                        }
+                    }).map((item,i)=>(
+                    <Card className="card" style={{ width: '19rem', height:'9rem' }}>
+                        <Card.Body className="card-body">
+                            <div className="user-img">
+                                <p className="person-icon"><Person /></p>
+                            </div>
+                            <div className="user-details">
+                            <Card.Title>{item.userName}</Card.Title>
+                            <Card.Text className="text">
+                                {item.handle}, work at {item.work},
+                                bio {item.bio}
+                            </Card.Text>
+                            </div>
+                        </Card.Body> 
+                    </Card>
+                    ))}                               
                 </div>
             </React.Fragment>
         )
+    
     }
    
-}
+// }
 
 export default CardSection;
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,14 +1,22 @@
 import CardSection from './CardSection.js';
-
+import { useState} from "react";
+import Logo from './github_logo.png'
+import { Search, Close} from '@material-ui/icons';
+import './SearchPage.css'
 
 function SearchPage(){
+    const [searchTerm,setSearchTerm] = useState('');
     return(
         <div className="search-container">
-            <div>
-                <h1>GitHub Profile Viewer</h1>
-                <p>Searchbar</p>
+            <div className="header-section">
+                <p><img src={Logo} alt="github logo"/>GitHub Profile Viewer</p>
+                <div className="search-section">
+                    <button><Search /></button>
+                    <input type="text" placeholder="Search user" value={searchTerm} onChange={event => {setSearchTerm(event.target.value)}}/>
+                    <button onClick={()=>{setSearchTerm("")}}><Close /></button>
+                </div>
             </div>
-            <CardSection />
+            <CardSection term={searchTerm}/>
         </div>
     )
 }
