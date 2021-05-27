192066a06068d28c0983f279d7989a4ce667986c
KeziaAnnMathew <keziaann26@gmail.com>
2021-05-27 23:35:27 +0530

    Fetch from api

--- src/Components/CardSection.js
+++ src/Components/CardSection.js
@@ -3,40 +3,45 @@ import {Card} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import {Person} from '@material-ui/icons';
 import Users from '../Users.json';
+import useProfileSearch from './useProfileSearch';
 import './CardSection.css';
 
 // class CardSection extends Component {
-    const CardSection= (props) => {          
+    const CardSection= (props) => {    
+
+
+        const {
+            loading, 
+            hasMore, 
+            usersList, 
+            error
+        } = useProfileSearch(props.term,props.pageNumber)      
         return(
             <React.Fragment>
-                    <div className="card-container">
-                        {Users.users.filter((val)=>{
-                            if(props.term === ""){
-                                return val;
-                            }
-                            else if(val.userName.toLowerCase().includes(props.term.toLowerCase())){
-                                return val;
-                            }
-                        }).map((item)=>(
-                        <Card className="card" style={{ width: '19rem', height:'9rem' }} key={item.id}>
-                            <Link to={`/${item.id}`} style={{ textDecoration: 'none' }}>
-                                <Card.Body className="card-body">
-                                    <div className="user-img">
-                                        <p className="person-icon"><Person /></p>
-                                    </div>
-                                    <div className="user-details">
-                                    <Card.Title>{item.userName}</Card.Title>
-                                    <Card.Text className="text">
-                                        {item.handle}, work at {item.work},
-                                        bio {item.bio}
-                                    </Card.Text>
-                                    </div>
-                                </Card.Body> 
-                            </Link>
-                        </Card>
-                        ))}                               
+                    <div>
+                        <div className="card-container">
+                            {usersList.map((item)=>(
+                            <Card className="card" style={{ width: '19rem', height:'9rem' }} key={item.login}>
+                                {/* <Link to={`/${item.id}`} style={{ textDecoration: 'none' }}> */}
+                                    <Card.Body className="card-body">
+                                        <div className="user-img">
+                                            <p className="person-icon"><Person /></p>
+                                        </div>
+                                        <div className="user-details">
+                                        <Card.Title>{item.login}</Card.Title>
+                                        <Card.Text className="text">
+                                            @{item.login}, work at ,
+                                            bio 
+                                        </Card.Text>
+                                        </div>
+                                    </Card.Body> 
+                                {/* </Link> */}
+                            </Card>
+                            ))}                               
+                        </div>
+                        <div>{loading && 'Loading...'}</div>
+                        <div>{error && "Error"}</div>
                     </div>
-
             </React.Fragment>
         )
     
@@ -45,3 +50,14 @@ import './CardSection.css';
 // }
 
 export default CardSection;
+
+
+
+// .filter((val)=>{
+//     if(props.term === ""){
+//         return val;
+//     }
+//     else if(val.userName.toLowerCase().includes(props.term.toLowerCase())){
+//         return val;
+//     }
+// })
--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -6,17 +6,29 @@ import './SearchPage.css'
 
 function SearchPage(){
     const [searchTerm,setSearchTerm] = useState('');
+    const [pageNUmber,setPageNumber] = useState(1);
+
+
+
+function handleSearch(e){
+    setSearchTerm(e.target.value);
+    setPageNumber(1)
+}
+
     return(
         <div className="search-container">
             <div className="header-section">
                 <p><img src={Logo} alt="github logo"/>GitHub Profile Viewer</p>
                 <div className="search-section">
                     <button><Search /></button>
-                    <input type="text" placeholder="Search user" value={searchTerm} onChange={event => {setSearchTerm(event.target.value)}}/>
+                    <input type="text" placeholder="Search user" value={searchTerm} onChange={handleSearch}/>
                     <button onClick={()=>{setSearchTerm("")}}><Close /></button>
                 </div>
             </div>
-            <CardSection term={searchTerm}/>
+            <CardSection 
+            term={searchTerm}
+            pageNumber={pageNUmber}
+            />
         </div>
     )
 }
